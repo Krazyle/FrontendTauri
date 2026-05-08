030de9fdff588bd3c66ff85a2c5968e5fe44509d
Prinz Edward Ducyogen <ducyogenpred@gmail.com>
2026-05-08 09:42:37 +0800

    Refactor raise_project_not_found function to remove project_id parameter

--- src-fastapi/projects/service.py
+++ src-fastapi/projects/service.py
@@ -21,29 +21,29 @@ class ProjectService:
     async def get(self, project_id: UUID) -> Project:
         project = await self.repository.get(project_id)
         if project is None:
-            raise_project_not_found(project_id)
+            raise_project_not_found()
         return project
 
     async def replace(self, project_id: UUID, project: ProjectReplace) -> Project:
         updated_project = await self.repository.replace(project_id, project)
 
         if updated_project is None:
-            raise_project_not_found(project_id)
+            raise_project_not_found()
         return updated_project
 
     async def update(self, project_id: UUID, project: ProjectUpdate) -> Project:
         updated_project = await self.repository.update(project_id, project)
 
         if updated_project is None:
-            raise_project_not_found(project_id)
+            raise_project_not_found()
         return updated_project
 
     async def delete(self, project_id: UUID) -> None:
         if not await self.repository.delete(project_id):
-            raise_project_not_found(project_id)
+            raise_project_not_found()
 
 
-def raise_project_not_found(project_id: UUID) -> NoReturn:
+def raise_project_not_found() -> NoReturn:
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Project not found",

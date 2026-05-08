a53a8ee426232c89321f06f678e6f66fe0f82019
Prinz Edward Ducyogen <ducyogenpred@gmail.com>
2026-05-08 14:23:45 +0800

    Add routers for collections, items, and proxy to main application

--- src-fastapi/main.py
+++ src-fastapi/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
 from projects import projects_router
+from collections import collections_router
+from items import items_router
+from proxy import proxy_router
 
 app = FastAPI()
 
 app.include_router(projects_router)
+app.include_router(collections_router)
+app.include_router(items_router)
+app.include_router(proxy_router)
 
 
 @app.get("/")
